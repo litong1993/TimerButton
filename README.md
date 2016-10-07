1b1d8ac06104a742b9d43c52b14316cd63ec3529
李童 <litong@litongdeMacBook-Pro.local>
2016-10-07 12:08:05 +0800

    提交说明文档

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# TimerButton
-倒计时按钮
+#TimerButton
+====
+点击后显示倒计时的按钮，可用来做发送验证码的按钮。
+![](/TimerButtton.gif)
+
+## 使用
+
+```swift
+override func viewDidLoad() {
+super.viewDidLoad()
+// Do any additional setup after loading the view, typically from a nib.
+let timerBtn = TimerButton(type: .custom)
+//设置按钮默认标题
+timerBtn.setTitle("点击发送验证码", for: .normal)
+timerBtn.frame = CGRect(x: 0, y: 300, width: 150, height: 50)
+//设置倒计时格式
+timerBtn.noEnableFormateString = "%@秒"
+//设置可用状态标题
+timerBtn.enableString = "重发"
+//设置倒计时间
+timerBtn.interval = 3
+timerBtn.backgroundColor = UIColor.green
+//设置delegate
+timerBtn.delegate = self
+self.view.addSubview(timerBtn)
+}
+```
+
+## delegate
+
+```swift
+//代理方法
+func timerBtnTouchUpInside() {
+print("发送")
+}
+```
+
+
--- TimerButton/TimerButton/ViewController.swift
+++ TimerButton/TimerButton/ViewController.swift
@@ -14,12 +14,17 @@ class ViewController: UIViewController,TimerButtonDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let timerBtn = TimerButton(type: .custom)
+        //设置按钮默认标题
         timerBtn.setTitle("点击发送验证码", for: .normal)
         timerBtn.frame = CGRect(x: 0, y: 300, width: 150, height: 50)
+        //设置倒计时格式
         timerBtn.noEnableFormateString = "%@秒"
+        //设置可用状态标题
         timerBtn.enableString = "重发"
+        //设置倒计时间
         timerBtn.interval = 3
         timerBtn.backgroundColor = UIColor.green
+        //设置delegate
         timerBtn.delegate = self
         self.view.addSubview(timerBtn)
     }
@@ -28,6 +33,7 @@ class ViewController: UIViewController,TimerButtonDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    //代理方法
     func timerBtnTouchUpInside() {
         print("发送")
     }
